7ba176c54b869cd645e351f4fbbec71466be3dc9
leanhnhan147 <anhnhan9a3@gmail.com>
2023-09-26 19:55:28 +0700

    initial project

---
+++ src/main/java/com/example/backendsimplemovies/BackendSimpleMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsimplemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSimpleMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSimpleMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendsimplemovies/BackendSimpleMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendsimplemovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSimpleMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
